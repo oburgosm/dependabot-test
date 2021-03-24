b223c4cbbb98c36100253d9a25b2ad73cbf474ba
Oscar Burgos Martin <oscarbma@ext.inditex.com>
2021-03-24 18:03:01 +0100

    dependabot-test

---
+++ dependabot-tomcat/src/main/java/com/bracso/dependabot/tomcat/DemoApplication.java
@@ -0,0 +1,23 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * Copyright (c) 2021.  Inditex 
+ */
+
+/**
+ *
+ * @author <a href="mailto:oscarbma@ext.inditex.com">Óscar Burgos Martín</a>
+ * @since 
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+        
+        
+
+}
